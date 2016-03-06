4f2df1e0c8a0a600dcd918accc5d5178be17c270
Ognjen Dragoljevic <ognjen.dragoljevic@gmail.com>
2016-03-06 14:36:38 +0000

    Collections

---
+++ include/algorithm/collections/collections.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <iterator>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+namespace altruct {
+namespace collections {
+
+/**
+ * Returns the vector of the elements in the range [begin, end) transformed by functor `f`.
+ */
+template<
+	typename It,
+	typename F,
+	typename T = typename std::result_of<F(typename std::iterator_traits<It>::value_type)>::type
+>
+std::vector<T> transform(It begin, It end, const F& f) {
+	std::vector<T> r;
+	for (It it = begin; it != end; ++it) {
+		r.push_back(f(*it));
+	}
+	return r;
+}
+
+/**
+ * Returns the vector of the elements in collection `c` transformed by functor `f`.
+ */
+template<
+	typename C,
+	typename F,
+	typename T = typename std::result_of<F(typename C::value_type)>::type
+>
+std::vector<T> transform(const C& c, const F& f) {
+	return transform(c.begin(), c.end(), f);
+}
+
+/**
+ * Returns the Run-Length encoding of the elements in the range [begin, end).
+ */
+template<
+	typename It,
+	typename T = typename std::iterator_traits<It>::value_type
+>
+std::vector<std::pair<T, int>> run_length(It begin, It end) {
+	std::vector<std::pair<T, int>> r;
+	for (It it = begin; it != end; ++it) {
+		if (!r.empty() && r.back().first == *it) {
+			r.back().second++;
+		} else {
+			r.push_back({ *it, 1 });
+		}
+	}
+	return r;
+}
+
+/**
+ * Returns the Run-Length encoding of the elements in collection `c`.
+ */
+template<
+	typename C,
+	typename T = typename C::value_type
+>
+std::vector<std::pair<T, int>> run_length(const C& c) {
+	return run_length(c.begin(), c.end());
+}
+
+} // collections
+} // altruct
---
+++ test/algorithm/collections/collections_test.cpp
@@ -0,0 +1,21 @@
+﻿#include "algorithm/collections/collections.h"
+
+#include "gtest/gtest.h"
+
+#include <string>
+#include <set>
+#include <vector>
+
+using namespace std;
+using namespace altruct::collections;
+
+TEST(reduce_test, transform) {
+	EXPECT_EQ((vector<int>{}), transform(vector<int>(), [](int x){return x*x; }));
+	EXPECT_EQ((vector<int>{4, 9, 25, 49}), transform(vector<int>{-2, 3, 5, 7}, [](int x){return x*x; }));
+	EXPECT_EQ((vector<int>{4, 9, 25, 49}), transform(set<int>{-2, 3, 5, 7}, [](int x){return x*x; }));
+}
+
+TEST(reduce_test, run_length) {
+	EXPECT_EQ((vector<pair<string, int>>{}), run_length(vector<string>()));
+	EXPECT_EQ((vector<pair<string, int>>{{ "a", 3 }, { "b", 1 }, { "c", 2 }, { "a", 1 }}), run_length(vector<string>{"a", "a", "a", "b", "c", "c", "a"}));
+}
