6221998b05f1b106a78b36c25b1d2b8334861908
Ognjen Dragoljevic <ognjen.dragoljevic@gmail.com>
2016-03-21 21:36:07 +0000

    Concurrency LOCK macro

---
+++ include/concurrency/concurrency.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <condition_variable>
+
+namespace altruct {
+namespace concurrency {
+
+#define LOCK(_mutex) \
+	for (bool first = true; first;) \
+		for (std::lock_guard<std::mutex> lock(_mutex); first; first = false)
+			// { /* locked */  }
+
+} // concurrency 
+} // altruct
---
+++ test/concurrency/concurrency_test.cpp
@@ -0,0 +1,24 @@
+﻿#include "concurrency/concurrency.h"
+
+#include "gtest/gtest.h"
+
+#include <string>
+#include <set>
+#include <vector>
+#include <ppl.h>
+
+using namespace std;
+using namespace concurrency;
+using namespace altruct::concurrency;
+
+typedef long long ll;
+
+TEST(concurrency_test, lock_macro) {
+	ll r = 0;
+	mutex r_mutex;
+	parallel_for(0, 10000, [&](ll i) {
+		ll q = i * i;
+		LOCK(r_mutex) { r += q; }
+	});
+	EXPECT_EQ(333283335000LL, r);
+}
