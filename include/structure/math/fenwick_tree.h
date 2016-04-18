2cdbaff5f5be493d33c41f000e5422e7ea81dd63
Ognjen Dragoljevic <ognjen.dragoljevic@gmail.com>
2016-04-18 05:37:40 +0100

    fenwick tree

---
+++ include/structure/math/fenwick_tree.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <vector>
+#include <functional>
+
+namespace altruct {
+namespace math {
+
+/**
+ * Fenwick tree.
+ */
+template<typename T>
+struct fenwick_tree {
+	std::vector<T> v;
+	std::function<T(T,T)> f;
+
+	fenwick_tree(int sz, std::function<T(T, T)> f) : v(sz + 1), f(f) {
+	}
+
+	void add(size_t index, const T& val) {
+		for (index++; index < v.size(); index += index & -index) v[index] = f(v[index], val);
+	}
+
+	T get_sum(size_t index) {
+		T r = 0;
+		for (index++; index > 0; index -= index & -index) r = f(r, v[index]);
+		return r;
+	}
+};
+
+} // math
+} // altruct
---
+++ test/structure/math/fenwick_tree_test.cpp
@@ -0,0 +1,24 @@
+﻿#include "algorithm/math/base.h"
+#include "structure/math/fenwick_tree.h"
+
+#include "gtest/gtest.h"
+
+#include <vector>
+
+using namespace std;
+using namespace altruct::math;
+
+TEST(fenwick_tree_test, fenwick_tree_sum) {
+	int n = 10;
+	fenwick_tree<int> f(n + 1, [](int r, int v){return r + v; });
+	for (int i = 0; i <= n; i++) {
+		f.add(i, sqT(i + 1));
+	}
+	f.add(5, 100);
+	f.add(2, -10);
+	vector<int> va(n + 1);
+	for (int i = 0; i <= n; i++) {
+		va[i] = f.get_sum(i);
+	}
+	EXPECT_EQ((vector<int>{1, 5, 4, 20, 45, 181, 230, 294, 375, 475, 596}), va);
+}
