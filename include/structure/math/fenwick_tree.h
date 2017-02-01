428bd3c83c82d9d87c28eca6cd99ae4676113521
Ognjen Dragoljevic <ognjen.dragoljevic@gmail.com>
2017-02-01 20:33:04 +0000

    fenwick tree

--- include/structure/math/fenwick_tree.h
+++ include/structure/math/fenwick_tree.h
@@ -14,15 +14,19 @@ struct fenwick_tree {
 	std::vector<T> v;
 	std::function<T(T,T)> f;
 
-	fenwick_tree(size_t sz, std::function<T(T, T)> f) : v(sz + 1), f(f) {
-	}
+    fenwick_tree(size_t sz, std::function<T(T, T)> f, T id = 0) : v(sz + 1, id), f(f) {
+    }
+
+    void reset(T id = 0) {
+        v.assign(v.size(), id);
+    }
 
 	void add(size_t index, const T& val) {
 		for (index++; index < v.size(); index += index & -index) v[index] = f(v[index], val);
 	}
 
-	T get_sum(size_t index) {
-		T r = 0;
+	T get_sum(size_t index, T id = 0) {
+		T r = id;
 		for (index++; index > 0; index -= index & -index) r = f(r, v[index]);
 		return r;
 	}
--- test/structure/math/fenwick_tree_test.cpp
+++ test/structure/math/fenwick_tree_test.cpp
@@ -21,4 +21,31 @@ TEST(fenwick_tree_test, fenwick_tree_sum) {
 		va[i] = f.get_sum(i);
 	}
 	EXPECT_EQ((vector<int>{1, 5, 4, 20, 45, 181, 230, 294, 375, 475, 596}), va);
+
+    f.reset();
+    for (int i = 0; i <= n; i++) {
+        va[i] = f.get_sum(i);
+    }
+    EXPECT_EQ((vector<int>(n + 1, 0)), va);
+}
+
+TEST(fenwick_tree_test, fenwick_tree_max) {
+    int n = 10;
+    fenwick_tree<int> f(n + 1, [](int r, int v){return std::max(r, v); }, -1000000000);
+    for (int i = 0; i <= n; i++) {
+        f.add(i, sqT(i + 1) * ((i % 2) ? +1 : -1));
+    }
+    f.add(5, 50);
+    f.add(2, -10);
+    vector<int> va(n + 1);
+    for (int i = 0; i <= n; i++) {
+        va[i] = f.get_sum(i, -1000000000);
+    }
+    EXPECT_EQ((vector<int>{-1, 4, 4, 16, 16, 50, 50, 64, 64, 100, 100}), va);
+
+    f.reset(-1000000000);
+    for (int i = 0; i <= n; i++) {
+        va[i] = f.get_sum(i, -1000000000);
+    }
+    EXPECT_EQ((vector<int>(n + 1, -1000000000)), va);
 }
