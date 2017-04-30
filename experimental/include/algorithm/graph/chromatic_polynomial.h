3f213671040e8315293aa7fa5b5e3f5e4e600c69
Ognjen Dragoljevic <ognjen.dragoljevic@gmail.com>
2017-04-30 14:24:21 +0100

    graph chromatic_polynomial

--- experimental/include/algorithm/graph/chromatic_polynomial.h
+++ experimental/include/algorithm/graph/chromatic_polynomial.h
@@ -34,11 +34,15 @@ altruct::math::polynom<I> chromatic_polynomial_K(int n, I id = I(1)) {
 }
 
 /**
- * Calculates the chromatic polynomial of an undirected graph.
+ * Calculates the chromatic polynomial of an undirected simple graph.
  *
- * Complexity: O(phi^n)
+ * Graph must be:
+ *   undirected: for each edge (u, v), there must be a corresponding edge (v, u).
+ *   simple: there should be no self loops and each edge (u, v) should occur at most once.
  *
- * @param g - an undirected graph with `n` nodes and `m` edges
+ * Complexity: O(phi^(n+m))
+ *
+ * @param g - an undirected simple graph with `n` nodes and `m` edges
  * @return - chromatic polynomial of the graph 'g'
  */
 template<typename I, typename E>
@@ -47,24 +51,15 @@ altruct::math::polynom<I> chromatic_polynomial(const graph<E>& g, I id = I(1)) {
     int n = g.size();
     poly k = { altruct::math::zeroOf(id), id };
     poly k1 = { -id, id };
-    // Delete vertex that is connected to every other vertex (if it exists)
+    // Delete vertex that is connected to every other vertex (if exists)
     for (int u = 0; u < n; u++) {
         if (g[u].size() < n - 1) continue;
-        std::vector<int> b(n);
-        int cnt = 0;
-        for (const E& e : g[u]) {
-            if (!b[e.v]) b[e.v] = 1, cnt++;
-        }
-        if (cnt < n - 1) continue;
         auto gd = g; gd.delete_node(u);
         return k * chromatic_polynomial<I>(gd)(k1);
     }
     // Handle each biconnected component independently
     auto d = chain_decomposition(g);
-    int C = (int)d.size(), E2 = 0, CE = 0;
-    for (int u = 0; u < n; u++) {
-        E2 += (int)g[u].size(); // this counts each edge twice
-    }
+    int C = (int)d.size(), AE = g.num_edges() / 2, CE = 0;
     std::vector<graph<edge>> vg;
     std::vector<int> idx(n, -1);
     for (const auto& vc : d) {
@@ -82,20 +77,34 @@ altruct::math::polynom<I> chromatic_polynomial(const graph<E>& g, I id = I(1)) {
             }
         }
     }
-    if (vg.size() != 1) {
-        poly p = powT(k, C) * powT(k1, E2 / 2 - CE);
+    if (vg.size() != 1 || C != 1 || AE != CE) {
+        poly p = powT(k, C) * powT(k1, AE - CE);
         for (const auto& gg : vg) {
             p *= chromatic_polynomial<I>(gg) / k;
         }
         return p;
     } else {
-        // Deletion–contraction recursion
-        // select arbitrary edge (TODO: use better heuristics)
-        int u = -1; for (u = 0; u < n && g[u].empty(); u++);
-        int v = g[u].back().v;
-        auto gd = g; gd.delete_edge(u, v); gd.delete_edge(v, u);
-        auto gc = g; gc.contract(u, v);
-        return chromatic_polynomial<I>(gd) -chromatic_polynomial<I>(gc);
+        // if reached this point, the graph consists of a single biconnected
+        // component and no vertex is connected to all other vertices.
+        // solve recursively
+        if (AE < n * n / 4) {
+            // sparse: deletion-contraction
+            // delete edge incident to the smallest degree vertex
+            int u = 0; for (int i = 0; i < n; i++) if (g[u].size() > g[i].size()) u = i;
+            int v = -1; for (const E& e : g[u]) if (v < 0 || g[v].size() > g[e.v].size()) v = e.v;
+            auto gd = g; gd.delete_edge(u, v); gd.delete_edge(v, u);
+            auto gc = g; gc.contract(u, v);
+            return chromatic_polynomial<I>(gd) - chromatic_polynomial<I>(gc);
+        } else {
+            // dense: addition-contraction
+            // add edge incident to the highest degree vertex
+            int u = 0; for (int i = 0; i < n; i++) if (g[u].size() < g[i].size()) u = i;
+            vector<int> used(n); used[u] = 1; for (const E& e : g[u]) used[e.v] = 1;
+            int v = -1; for (int i = 0; i < n; i++) if ((v < 0 || g[v].size() < g[i].size()) && !used[i]) v = i;
+            auto ga = g; ga.add_edge(u, v); ga.add_edge(v, u);
+            auto gc = g; gc.contract(u, v);
+            return chromatic_polynomial<I>(ga) + chromatic_polynomial<I>(gc);
+        }
     }
 }
 
