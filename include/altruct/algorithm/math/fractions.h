227eda4ad2efba0c339f0ab07e5da88edaa0225f
Ognjen Dragoljevic <ognjen.dragoljevic@gmail.com>
2020-10-25 15:37:38 +0000

    Add repeating_decimal & rational_digits

--- include/altruct/algorithm/math/fractions.h
+++ include/altruct/algorithm/math/fractions.h
@@ -2,6 +2,7 @@
 
 #include "altruct/algorithm/math/base.h"
 #include "altruct/structure/math/fraction.h"
+#include "altruct/structure/math/modulo.h"
 
 #include <vector>
 #include <algorithm>
@@ -24,6 +25,57 @@ fraction<I> farey_neighbour(const I& n, const fraction<I>& f_prev, const fractio
     }
     I k = (n + q) / f.q;
     return fraction<I>(k * f.p - p, k * f.q - q);
+}
+
+/**
+ * Decimal expansion of a fraction p/q in base b.
+ * Requirs 0 < p/q < 1 and b >= 2.
+ */
+template<typename I>
+std::pair<std::vector<int>, size_t> repeating_decimal(int b, I p, I q) {
+    std::vector<int> digits;
+    size_t pos = 0;
+    std::map<I, size_t> positions;
+    while (!positions.count(p)) {
+        positions[p] = pos;
+        I bp = b * p;
+        int d = castOf<int, I>(bp / q);
+        digits.push_back(d);
+        p = bp - d * q;
+        if (p == 0) {
+            return { digits, 0 };
+        }
+        pos += 1;
+    }
+    return { digits, pos - positions[p] };
+}
+
+/**
+ * Gives n-th digit after decimal point in base b of a fraction p/q.
+ * Requires 0 < p/q < 1.
+ */
+template<typename I>
+int rational_digit(I n, int b, I p, I q, I* r = nullptr) {
+    using modx = moduloX<I>;
+    p = (powT(modx(b, q), n) * modx(p, q)).v;
+    if (r) *r = p;
+    return castOf<int, I>((p * b) / q);
+}
+
+/**
+ * Gives digits n to n+len after decimal point in base b of a fraction p/q.
+ * Requires 0 < p/q < 1.
+ */
+template<typename I>
+std::vector<int> rational_digits(I n, int len, int b, I p, I q) {
+    std::vector<int> digits;
+    if (len-- > 0) {
+        digits.push_back(rational_digit<I>(n, b, p, q, &p));
+    }
+    while (len-- > 0) {
+        digits.push_back(rational_digit<I>(1, b, p, q, &p));
+    }
+    return digits;
 }
 
 } // math
--- test/algorithm/math/fractions_test.cpp
+++ test/algorithm/math/fractions_test.cpp
@@ -48,3 +48,39 @@ TEST(fractions_test, farey_sequence_dec) {
     }
     EXPECT_EQ((vector<frac>{{ 1, 1 }, { 4, 5 }, { 3, 4 }, { 2, 3 }, { 3, 5 }, { 1, 2 }, { 2, 5 }, { 1, 3 }, { 1, 4 }, { 1, 5 }, { 0, 1 }}), vf);
 }
+
+TEST(fractions_test, repeating_decimal) {
+    EXPECT_EQ(make_pair(std::vector<int>{5}, size_t(0)), repeating_decimal(10, 1, 2));
+    EXPECT_EQ(make_pair(std::vector<int>{3}, size_t(1)), repeating_decimal(10, 1, 3));
+    EXPECT_EQ(make_pair(std::vector<int>{2, 5}, size_t(0)), repeating_decimal(10, 1, 4));
+    EXPECT_EQ(make_pair(std::vector<int>{0, 1}, size_t(0)), repeating_decimal(10, 1, 100));
+    EXPECT_EQ(make_pair(std::vector<int>{1, 4, 2, 8, 5, 7}, size_t(6)), repeating_decimal(10, 1, 7));
+    EXPECT_EQ(make_pair(std::vector<int>{8, 3, 1, 4, 2, 8, 5, 7}, size_t(6)), repeating_decimal(10, 291, 350));
+}
+
+TEST(fractions_test, rational_digit) {
+    EXPECT_EQ(2, rational_digit(0, 10, 1, 4));
+    EXPECT_EQ(5, rational_digit(1, 10, 1, 4));
+    EXPECT_EQ(0, rational_digit(2, 10, 1, 4));
+    EXPECT_EQ(0, rational_digit(3, 10, 1, 4));
+    EXPECT_EQ(8, rational_digit(0, 10, 291, 350));
+    EXPECT_EQ(3, rational_digit(1, 10, 291, 350));
+    EXPECT_EQ(1, rational_digit(2, 10, 291, 350));
+    EXPECT_EQ(4, rational_digit(3, 10, 291, 350));
+    EXPECT_EQ(2, rational_digit(4, 10, 291, 350));
+    EXPECT_EQ(8, rational_digit(5, 10, 291, 350));
+    EXPECT_EQ(5, rational_digit(6, 10, 291, 350));
+    EXPECT_EQ(7, rational_digit(7, 10, 291, 350));
+    EXPECT_EQ(1, rational_digit(8, 10, 291, 350));
+    EXPECT_EQ(4, rational_digit(9, 10, 291, 350));
+    EXPECT_EQ(2, rational_digit(10, 10, 291, 350));
+    EXPECT_EQ(8, rational_digit(11, 10, 291, 350));
+    EXPECT_EQ(5, rational_digit(12, 10, 291, 350));
+    EXPECT_EQ(7, rational_digit(13, 10, 291, 350));
+}
+
+TEST(fractions_test, rational_digits) {
+    EXPECT_EQ((std::vector<int>{ 3, 4, 5, 6, 7, 8, 9, 0, 5, 6, 7, 8, 9, 0, 5, 6, 7, 8, 9, 0, 5, 6, 7, 8, 9, 0, 5, 6, 7, 8 }), rational_digits<int64_t>(2, 30, 10, 77160416, 624999375));
+}
+    
+//cout << from_digits<int>(integer_digits(12345678, 10), 10) << endl;
