fdb1deab73819808db3e967bf1a37c6efcb8185d
Ognjen Dragoljevic <ognjen.dragoljevic@gmail.com>
2022-08-19 14:20:09 +0100

    binary_search

--- include/altruct/algorithm/search/binary_search.h
+++ include/altruct/algorithm/search/binary_search.h
@@ -3,58 +3,83 @@
 namespace altruct {
 namespace search {
 
+
 /**
- * Binary search
+ * Returns the first index for which `predicate` returns `true`.
+ *
+ * Predicate must be sorted so all `false` come before `true`.
+ * `It` must be a random-access iterator, pointer, or integral type.
  *
  * @param lo  - lower bound (inclusive)
  * @param hi  - upper bound (exclusive)
- * @param eps - searches until `(hi - lo) < eps`.
- *              Must be 1 for integral, pointer and iterator types.
- * @param f   - `f(mid)` returns `true` iff the result is bigger than `mid`.
- *              The following invariant must hold: `f(mid) = (mid < mid0)`
- *              for some `mid0` to be found.
- * @return    - the smallest `mid0` for which `f(mid0)` is `false`
+ * @return the smallest index for which `predicate` returns `true`.
  */
-template<typename T, typename D, typename F>
-T binary_search2(T lo, T hi, D eps, F f) {
-    T mid = lo, mid0 = hi;
-    while (mid != mid0 && (hi - lo) >= eps) {
-        mid0 = mid;
-        mid = lo + (hi - lo) / 2;
-        if (f(mid)) {
-            lo = mid + eps;
-        } else {
+template<typename It, typename F>
+It binary_search_pred(It lo, It hi, F predicate) {
+    while (lo < hi) {
+        auto mid = lo + (hi - lo) / 2;
+        if (predicate(mid)) {
             hi = mid;
+        } else {
+            lo = ++mid;
         }
     }
     return lo;
 }
 
 /**
- * Finds the smallest argument `x` in the range `[lo, hi)` for which `f(x) >= val`.
+ * Returns the first value for which `predicate` returns `true`.
  *
- * See `binary_search2` for more details.
+ * Predicate must be sorted so all `false` come before `true`.
+ * `V` must be a numerical type.
+ *
+ * @param lo  - lower bound (inclusive)
+ * @param hi  - upper bound (exclusive)
+ * @return the smallest value for which `predicate` returns `true`.
  */
-template<typename T, typename D, typename V, typename F>
-T lower_bound(T lo, T hi, D eps, F f, const V& val, bool decreasing = false) {
+template<typename X, typename F>
+X binary_search_num(X lo, X hi, X eps, F predicate) {
+    auto prev_mid = hi;
+    while ((hi - lo) >= eps) {
+        auto mid = lo + (hi - lo) / 2;
+        if (mid == prev_mid) break;
+        if (predicate(mid)) {
+            hi = mid;
+        } else {
+            lo = mid + eps;
+        }
+        prev_mid = mid;
+    }
+    return lo;
+}
+
+/**
+ * Finds the smallest argument `x` in the range `[lo, hi)` for which `f(x) >= val` when `f` is not decreasing,
+ * or `f(x) <= val` when `f` is decreasing.
+ *
+ * See `binary_search_num` for more details.
+ */
+template<typename X, typename Y, typename F>
+X lower_bound_num(X lo, X hi, X eps, const Y& val, F f, bool decreasing = false) {
     if (decreasing) {
-        return binary_search2(lo, hi, eps, [&](const T& mid){ return f(mid) > val; });
+        return binary_search_num(lo, hi, eps, [&](X mid){ return f(mid) <= val; });
     } else {
-        return binary_search2(lo, hi, eps, [&](const T& mid){ return f(mid) < val; });
+        return binary_search_num(lo, hi, eps, [&](X mid){ return f(mid) >= val; });
     }
 }
 
 /**
- * Finds the smallest argument `x` in the range `[lo, hi)` for which `f(x) > val`.
+ * Finds the smallest argument `x` in the range `[lo, hi)` for which `f(x) > val` when `f` is not decreasing,
+ * or `f(x) < val` when `f` is decreasing.
  *
- * See `binary_search2` for more details.
+ * See `binary_search_num` for more details.
  */
-template<typename T, typename D, typename V, typename F>
-T upper_bound(T lo, T hi, D eps, F f, const V& val, bool decreasing = false) {
+template<typename X, typename Y, typename F>
+X upper_bound_num(X lo, X hi, X eps, const Y& val, F f, bool decreasing = false) {
     if (decreasing) {
-        return binary_search2(lo, hi, eps, [&](const T& mid){ return f(mid) >= val; });
+        return binary_search_num(lo, hi, eps, [&](X mid){ return f(mid) < val; });
     } else {
-        return binary_search2(lo, hi, eps, [&](const T& mid){ return f(mid) <= val; });
+        return binary_search_num(lo, hi, eps, [&](X mid){ return f(mid) > val; });
     }
 }
 
--- test/algorithm/search/binary_search_test.cpp
+++ test/algorithm/search/binary_search_test.cpp
@@ -7,54 +7,111 @@
 using namespace std;
 using namespace altruct::search;
 
-TEST(binary_search_test, poly_zero) {
+TEST(binary_search_test, poly_zero_double) {
     auto p = [](double x) { return ((1 * x - 2) * x + 0) * x + 1; };
-    EXPECT_NEAR(-0.618033988749895, lower_bound(-1.0, 0.0, 0.0, p, 0.0, false), 1e-9);
-    EXPECT_NEAR(-0.618033988749895, upper_bound(-1.0, 0.0, 0.0, p, 0.0, false), 1e-9);
-    EXPECT_NEAR(+1.000000000000000, lower_bound(0.0, 4/3.0, 0.0, p, 0.0, true), 1e-9);
-    EXPECT_NEAR(+1.000000000000000, upper_bound(0.0, 4/3.0, 0.0, p, 0.0, true), 1e-9);
-    EXPECT_NEAR(+1.618033988749895, lower_bound(4/3.0, 2.0, 0.0, p, 0.0, false), 1e-9);
-    EXPECT_NEAR(+1.618033988749895, upper_bound(4/3.0, 2.0, 0.0, p, 0.0, false), 1e-9);
+    double eps = 0.0;
+    EXPECT_NEAR(-0.618033988749895, lower_bound_num(-1.0, 0.0, eps, 0.0, p, false), 1e-9);
+    EXPECT_NEAR(-0.618033988749895, upper_bound_num(-1.0, 0.0, eps, 0.0, p, false), 1e-9);
+    EXPECT_NEAR(+1.000000000000000, lower_bound_num(0.0, 4/3.0, eps, 0.0, p, true), 1e-9);
+    EXPECT_NEAR(+1.000000000000000, upper_bound_num(0.0, 4/3.0, eps, 0.0, p, true), 1e-9);
+    EXPECT_NEAR(+1.618033988749895, lower_bound_num(4/3.0, 2.0, eps, 0.0, p, false), 1e-9);
+    EXPECT_NEAR(+1.618033988749895, upper_bound_num(4/3.0, 2.0, eps, 0.0, p, false), 1e-9);
+}
+
+TEST(binary_search_test, poly_zero_double_eps) {
+    auto p = [](double x) { return ((1 * x - 2) * x + 0) * x + 1; };
+    double eps = 0.001;
+    EXPECT_NEAR(-0.618, lower_bound_num(-1.0, 0.0, eps, 0.0, p, false), eps);
+    EXPECT_NEAR(-0.618, upper_bound_num(-1.0, 0.0, eps, 0.0, p, false), eps);
+    EXPECT_NEAR(+1.000, lower_bound_num(0.0, 4 / 3.0, eps, 0.0, p, true), eps);
+    EXPECT_NEAR(+1.000, upper_bound_num(0.0, 4 / 3.0, eps, 0.0, p, true), eps);
+    EXPECT_NEAR(+1.618, lower_bound_num(4 / 3.0, 2.0, eps, 0.0, p, false), eps);
+    EXPECT_NEAR(+1.618, upper_bound_num(4 / 3.0, 2.0, eps, 0.0, p, false), eps);
 }
 
 TEST(binary_search_test, index) {
+    int eps = 1;
+
     int a0[] = { 0 };
+    auto deref_idx0 = [&](int idx) { return a0[idx]; };
+    EXPECT_EQ(0, binary_search_pred(0, 0, [&](int idx) { return a0[idx] >= 10; }));
+    EXPECT_EQ(0, lower_bound_num(0, 0, eps, 10, deref_idx0));
+    EXPECT_EQ(0, upper_bound_num(0, 0, eps, 10, deref_idx0));
+
     int a1[] = { 5 };
+    auto deref_idx1 = [&](int idx) { return a1[idx]; };
+    EXPECT_EQ(0, binary_search_pred(0, 1, [&](int idx) { return a1[idx] >= 4; }));
+    EXPECT_EQ(0, binary_search_pred(0, 1, [&](int idx) { return a1[idx] >= 5; }));
+    EXPECT_EQ(1, binary_search_pred(0, 1, [&](int idx) { return a1[idx] >= 6; }));
+    EXPECT_EQ(0, lower_bound_num(0, 1, eps, 4, deref_idx1));
+    EXPECT_EQ(0, lower_bound_num(0, 1, eps, 5, deref_idx1));
+    EXPECT_EQ(1, lower_bound_num(0, 1, eps, 6, deref_idx1));
+    EXPECT_EQ(0, binary_search_pred(0, 1, [&](int idx) { return a1[idx] > 4; }));
+    EXPECT_EQ(1, binary_search_pred(0, 1, [&](int idx) { return a1[idx] > 5; }));
+    EXPECT_EQ(1, binary_search_pred(0, 1, [&](int idx) { return a1[idx] > 6; }));
+    EXPECT_EQ(0, upper_bound_num(0, 1, eps, 4, deref_idx1));
+    EXPECT_EQ(1, upper_bound_num(0, 1, eps, 5, deref_idx1));
+    EXPECT_EQ(1, upper_bound_num(0, 1, eps, 6, deref_idx1));
+
     int a2[] = { 2, 2, 5, 5, 5, 5, 8 };
-    auto deref_idx0 = [&](int idx){ return a0[idx]; };
-    auto deref_idx1 = [&](int idx){ return a1[idx]; };
-    auto deref_idx2 = [&](int idx){ return a2[idx]; };
+    auto deref_idx2 = [&](int idx) { return a2[idx]; };
+    EXPECT_EQ(0, binary_search_pred(0, 7, [&](int idx) { return a2[idx] >= 1; }));
+    EXPECT_EQ(0, binary_search_pred(0, 7, [&](int idx) { return a2[idx] >= 2; }));
+    EXPECT_EQ(2, binary_search_pred(0, 7, [&](int idx) { return a2[idx] >= 3; }));
+    EXPECT_EQ(2, binary_search_pred(0, 7, [&](int idx) { return a2[idx] >= 4; }));
+    EXPECT_EQ(2, binary_search_pred(0, 7, [&](int idx) { return a2[idx] >= 5; }));
+    EXPECT_EQ(6, binary_search_pred(0, 7, [&](int idx) { return a2[idx] >= 6; }));
+    EXPECT_EQ(6, binary_search_pred(0, 7, [&](int idx) { return a2[idx] >= 7; }));
+    EXPECT_EQ(6, binary_search_pred(0, 7, [&](int idx) { return a2[idx] >= 8; }));
+    EXPECT_EQ(7, binary_search_pred(0, 7, [&](int idx) { return a2[idx] >= 9; }));
+    EXPECT_EQ(0, lower_bound_num(0, 7, eps, 1, deref_idx2));
+    EXPECT_EQ(0, lower_bound_num(0, 7, eps, 2, deref_idx2));
+    EXPECT_EQ(2, lower_bound_num(0, 7, eps, 3, deref_idx2));
+    EXPECT_EQ(2, lower_bound_num(0, 7, eps, 4, deref_idx2));
+    EXPECT_EQ(2, lower_bound_num(0, 7, eps, 5, deref_idx2));
+    EXPECT_EQ(6, lower_bound_num(0, 7, eps, 6, deref_idx2));
+    EXPECT_EQ(6, lower_bound_num(0, 7, eps, 7, deref_idx2));
+    EXPECT_EQ(6, lower_bound_num(0, 7, eps, 8, deref_idx2));
+    EXPECT_EQ(7, lower_bound_num(0, 7, eps, 9, deref_idx2));
+    EXPECT_EQ(0, binary_search_pred(0, 7, [&](int idx) { return a2[idx] > 1; }));
+    EXPECT_EQ(2, binary_search_pred(0, 7, [&](int idx) { return a2[idx] > 2; }));
+    EXPECT_EQ(2, binary_search_pred(0, 7, [&](int idx) { return a2[idx] > 3; }));
+    EXPECT_EQ(2, binary_search_pred(0, 7, [&](int idx) { return a2[idx] > 4; }));
+    EXPECT_EQ(6, binary_search_pred(0, 7, [&](int idx) { return a2[idx] > 5; }));
+    EXPECT_EQ(6, binary_search_pred(0, 7, [&](int idx) { return a2[idx] > 6; }));
+    EXPECT_EQ(6, binary_search_pred(0, 7, [&](int idx) { return a2[idx] > 7; }));
+    EXPECT_EQ(7, binary_search_pred(0, 7, [&](int idx) { return a2[idx] > 8; }));
+    EXPECT_EQ(7, binary_search_pred(0, 7, [&](int idx) { return a2[idx] > 9; }));
+    EXPECT_EQ(0, upper_bound_num(0, 7, eps, 1, deref_idx2));
+    EXPECT_EQ(2, upper_bound_num(0, 7, eps, 2, deref_idx2));
+    EXPECT_EQ(2, upper_bound_num(0, 7, eps, 3, deref_idx2));
+    EXPECT_EQ(2, upper_bound_num(0, 7, eps, 4, deref_idx2));
+    EXPECT_EQ(6, upper_bound_num(0, 7, eps, 5, deref_idx2));
+    EXPECT_EQ(6, upper_bound_num(0, 7, eps, 6, deref_idx2));
+    EXPECT_EQ(6, upper_bound_num(0, 7, eps, 7, deref_idx2));
+    EXPECT_EQ(7, upper_bound_num(0, 7, eps, 8, deref_idx2));
+    EXPECT_EQ(7, upper_bound_num(0, 7, eps, 9, deref_idx2));
 
-    EXPECT_EQ(0, lower_bound(0, 0, 1, deref_idx0, 10));
-    EXPECT_EQ(0, upper_bound(0, 0, 1, deref_idx0, 10));
-
-    EXPECT_EQ(0, lower_bound(0, 1, 1, deref_idx1, 4));
-    EXPECT_EQ(0, lower_bound(0, 1, 1, deref_idx1, 5));
-    EXPECT_EQ(1, lower_bound(0, 1, 1, deref_idx1, 6));
-
-    EXPECT_EQ(0, upper_bound(0, 1, 1, deref_idx1, 4));
-    EXPECT_EQ(1, upper_bound(0, 1, 1, deref_idx1, 5));
-    EXPECT_EQ(1, upper_bound(0, 1, 1, deref_idx1, 6));
-
-    EXPECT_EQ(0, lower_bound(0, 7, 1, deref_idx2, 1));
-    EXPECT_EQ(0, lower_bound(0, 7, 1, deref_idx2, 2));
-    EXPECT_EQ(2, lower_bound(0, 7, 1, deref_idx2, 3));
-    EXPECT_EQ(2, lower_bound(0, 7, 1, deref_idx2, 4));
-    EXPECT_EQ(2, lower_bound(0, 7, 1, deref_idx2, 5));
-    EXPECT_EQ(6, lower_bound(0, 7, 1, deref_idx2, 6));
-    EXPECT_EQ(6, lower_bound(0, 7, 1, deref_idx2, 7));
-    EXPECT_EQ(6, lower_bound(0, 7, 1, deref_idx2, 8));
-    EXPECT_EQ(7, lower_bound(0, 7, 1, deref_idx2, 9));
-
-    EXPECT_EQ(0, upper_bound(0, 7, 1, deref_idx2, 1));
-    EXPECT_EQ(2, upper_bound(0, 7, 1, deref_idx2, 2));
-    EXPECT_EQ(2, upper_bound(0, 7, 1, deref_idx2, 3));
-    EXPECT_EQ(2, upper_bound(0, 7, 1, deref_idx2, 4));
-    EXPECT_EQ(6, upper_bound(0, 7, 1, deref_idx2, 5));
-    EXPECT_EQ(6, upper_bound(0, 7, 1, deref_idx2, 6));
-    EXPECT_EQ(6, upper_bound(0, 7, 1, deref_idx2, 7));
-    EXPECT_EQ(7, upper_bound(0, 7, 1, deref_idx2, 8));
-    EXPECT_EQ(7, upper_bound(0, 7, 1, deref_idx2, 9));
+    int a3[] = { 8, 5, 5, 5, 5, 2, 2 };
+    auto deref_idx3 = [&](int idx) { return a3[idx]; };
+    EXPECT_EQ(7, lower_bound_num(0, 7, eps, 1, deref_idx3, true));
+    EXPECT_EQ(5, lower_bound_num(0, 7, eps, 2, deref_idx3, true));
+    EXPECT_EQ(5, lower_bound_num(0, 7, eps, 3, deref_idx3, true));
+    EXPECT_EQ(5, lower_bound_num(0, 7, eps, 4, deref_idx3, true));
+    EXPECT_EQ(1, lower_bound_num(0, 7, eps, 5, deref_idx3, true));
+    EXPECT_EQ(1, lower_bound_num(0, 7, eps, 6, deref_idx3, true));
+    EXPECT_EQ(1, lower_bound_num(0, 7, eps, 7, deref_idx3, true));
+    EXPECT_EQ(0, lower_bound_num(0, 7, eps, 8, deref_idx3, true));
+    EXPECT_EQ(0, lower_bound_num(0, 7, eps, 9, deref_idx3, true));
+    EXPECT_EQ(7, upper_bound_num(0, 7, eps, 1, deref_idx3, true));
+    EXPECT_EQ(7, upper_bound_num(0, 7, eps, 2, deref_idx3, true));
+    EXPECT_EQ(5, upper_bound_num(0, 7, eps, 3, deref_idx3, true));
+    EXPECT_EQ(5, upper_bound_num(0, 7, eps, 4, deref_idx3, true));
+    EXPECT_EQ(5, upper_bound_num(0, 7, eps, 5, deref_idx3, true));
+    EXPECT_EQ(1, upper_bound_num(0, 7, eps, 6, deref_idx3, true));
+    EXPECT_EQ(1, upper_bound_num(0, 7, eps, 7, deref_idx3, true));
+    EXPECT_EQ(1, upper_bound_num(0, 7, eps, 8, deref_idx3, true));
+    EXPECT_EQ(0, upper_bound_num(0, 7, eps, 9, deref_idx3, true));
 }
 
 TEST(binary_search_test, pointer) {
@@ -63,74 +120,73 @@ TEST(binary_search_test, pointer) {
     int a[] = { 2, 2, 5, 5, 5, 5, 8 };
     auto deref_ptr = [](int* ptr){ return *ptr; };
 
-    EXPECT_EQ(a0, lower_bound(a0, a0, 1, deref_ptr, 10));
-    EXPECT_EQ(a0, upper_bound(a0, a0, 1, deref_ptr, 10));
+    EXPECT_EQ(a0, binary_search_pred(a0, a0, [](int* ptr) { return *ptr >= 10; }));
+    EXPECT_EQ(a0, binary_search_pred(a0, a0, [](int* ptr) { return *ptr > 10; }));
 
-    EXPECT_EQ(a1, lower_bound(a1, a1 + 1, 1, deref_ptr, 4));
-    EXPECT_EQ(a1, lower_bound(a1, a1 + 1, 1, deref_ptr, 5));
-    EXPECT_EQ(a1 + 1, lower_bound(a1, a1 + 1, 1, deref_ptr, 6));
+    EXPECT_EQ(a1 + 0, binary_search_pred(a1, a1 + 1, [](int* ptr) { return *ptr >= 4; }));
+    EXPECT_EQ(a1 + 0, binary_search_pred(a1, a1 + 1, [](int* ptr) { return *ptr >= 5; }));
+    EXPECT_EQ(a1 + 1, binary_search_pred(a1, a1 + 1, [](int* ptr) { return *ptr >= 6; }));
 
-    EXPECT_EQ(a1, upper_bound(a1, a1 + 1, 1, deref_ptr, 4));
-    EXPECT_EQ(a1 + 1, upper_bound(a1, a1 + 1, 1, deref_ptr, 5));
-    EXPECT_EQ(a1 + 1, upper_bound(a1, a1 + 1, 1, deref_ptr, 6));
+    EXPECT_EQ(a1 + 0, binary_search_pred(a1, a1 + 1, [](int* ptr) { return *ptr > 4; }));
+    EXPECT_EQ(a1 + 1, binary_search_pred(a1, a1 + 1, [](int* ptr) { return *ptr > 5; }));
+    EXPECT_EQ(a1 + 1, binary_search_pred(a1, a1 + 1, [](int* ptr) { return *ptr > 6; }));
 
-    EXPECT_EQ(a + 0, lower_bound(a, a + 7, 1, deref_ptr, 1));
-    EXPECT_EQ(a + 0, lower_bound(a, a + 7, 1, deref_ptr, 2));
-    EXPECT_EQ(a + 2, lower_bound(a, a + 7, 1, deref_ptr, 3));
-    EXPECT_EQ(a + 2, lower_bound(a, a + 7, 1, deref_ptr, 4));
-    EXPECT_EQ(a + 2, lower_bound(a, a + 7, 1, deref_ptr, 5));
-    EXPECT_EQ(a + 6, lower_bound(a, a + 7, 1, deref_ptr, 6));
-    EXPECT_EQ(a + 6, lower_bound(a, a + 7, 1, deref_ptr, 7));
-    EXPECT_EQ(a + 6, lower_bound(a, a + 7, 1, deref_ptr, 8));
-    EXPECT_EQ(a + 7, lower_bound(a, a + 7, 1, deref_ptr, 9));
+    EXPECT_EQ(a + 0, binary_search_pred(a, a + 7, [](int* ptr) { return *ptr >= 1; }));
+    EXPECT_EQ(a + 0, binary_search_pred(a, a + 7, [](int* ptr) { return *ptr >= 2; }));
+    EXPECT_EQ(a + 2, binary_search_pred(a, a + 7, [](int* ptr) { return *ptr >= 3; }));
+    EXPECT_EQ(a + 2, binary_search_pred(a, a + 7, [](int* ptr) { return *ptr >= 4; }));
+    EXPECT_EQ(a + 2, binary_search_pred(a, a + 7, [](int* ptr) { return *ptr >= 5; }));
+    EXPECT_EQ(a + 6, binary_search_pred(a, a + 7, [](int* ptr) { return *ptr >= 6; }));
+    EXPECT_EQ(a + 6, binary_search_pred(a, a + 7, [](int* ptr) { return *ptr >= 7; }));
+    EXPECT_EQ(a + 6, binary_search_pred(a, a + 7, [](int* ptr) { return *ptr >= 8; }));
+    EXPECT_EQ(a + 7, binary_search_pred(a, a + 7, [](int* ptr) { return *ptr >= 9; }));
 
-    EXPECT_EQ(a + 0, upper_bound(a, a + 7, 1, deref_ptr, 1));
-    EXPECT_EQ(a + 2, upper_bound(a, a + 7, 1, deref_ptr, 2));
-    EXPECT_EQ(a + 2, upper_bound(a, a + 7, 1, deref_ptr, 3));
-    EXPECT_EQ(a + 2, upper_bound(a, a + 7, 1, deref_ptr, 4));
-    EXPECT_EQ(a + 6, upper_bound(a, a + 7, 1, deref_ptr, 5));
-    EXPECT_EQ(a + 6, upper_bound(a, a + 7, 1, deref_ptr, 6));
-    EXPECT_EQ(a + 6, upper_bound(a, a + 7, 1, deref_ptr, 7));
-    EXPECT_EQ(a + 7, upper_bound(a, a + 7, 1, deref_ptr, 8));
-    EXPECT_EQ(a + 7, upper_bound(a, a + 7, 1, deref_ptr, 9));
+    EXPECT_EQ(a + 0, binary_search_pred(a, a + 7, [](int* ptr) { return *ptr > 1; }));
+    EXPECT_EQ(a + 2, binary_search_pred(a, a + 7, [](int* ptr) { return *ptr > 2; }));
+    EXPECT_EQ(a + 2, binary_search_pred(a, a + 7, [](int* ptr) { return *ptr > 3; }));
+    EXPECT_EQ(a + 2, binary_search_pred(a, a + 7, [](int* ptr) { return *ptr > 4; }));
+    EXPECT_EQ(a + 6, binary_search_pred(a, a + 7, [](int* ptr) { return *ptr > 5; }));
+    EXPECT_EQ(a + 6, binary_search_pred(a, a + 7, [](int* ptr) { return *ptr > 6; }));
+    EXPECT_EQ(a + 6, binary_search_pred(a, a + 7, [](int* ptr) { return *ptr > 7; }));
+    EXPECT_EQ(a + 7, binary_search_pred(a, a + 7, [](int* ptr) { return *ptr > 8; }));
+    EXPECT_EQ(a + 7, binary_search_pred(a, a + 7, [](int* ptr) { return *ptr > 9; }));
 }
 
 TEST(binary_search_test, iterator) {
     vector<int> v0{};
     vector<int> v1{ 5 };
     vector<int> v{ 2, 2, 5, 5, 5, 5, 8 };
-    auto deref_it = [](vector<int>::iterator it){ return *it; };
 
-    EXPECT_EQ(v0.end(), lower_bound(v0.begin(), v0.end(), 1, deref_it, 10));
-    EXPECT_EQ(v0.end(), upper_bound(v0.begin(), v0.end(), 1, deref_it, 10));
+    EXPECT_EQ(v0.end(), binary_search_pred(v0.begin(), v0.end(), [](const vector<int>::iterator& it) { return *it >= 10; }));
+    EXPECT_EQ(v0.end(), binary_search_pred(v0.begin(), v0.end(), [](const vector<int>::iterator& it) { return *it > 10; }));
 
-    EXPECT_EQ(v1.begin(), lower_bound(v1.begin(), v1.end(), 1, deref_it, 4));
-    EXPECT_EQ(v1.begin(), lower_bound(v1.begin(), v1.end(), 1, deref_it, 5));
-    EXPECT_EQ(v1.end(), lower_bound(v1.begin(), v1.end(), 1, deref_it, 6));
+    EXPECT_EQ(v1.begin(), binary_search_pred(v1.begin(), v1.end(), [](const vector<int>::iterator& it) { return *it >= 4; }));
+    EXPECT_EQ(v1.begin(), binary_search_pred(v1.begin(), v1.end(), [](const vector<int>::iterator& it) { return *it >= 5; }));
+    EXPECT_EQ(v1.end(), binary_search_pred(v1.begin(), v1.end(), [](const vector<int>::iterator& it) { return *it >= 6; }));
 
-    EXPECT_EQ(v1.begin(), upper_bound(v1.begin(), v1.end(), 1, deref_it, 4));
-    EXPECT_EQ(v1.end(), upper_bound(v1.begin(), v1.end(), 1, deref_it, 5));
-    EXPECT_EQ(v1.end(), upper_bound(v1.begin(), v1.end(), 1, deref_it, 6));
+    EXPECT_EQ(v1.begin(), binary_search_pred(v1.begin(), v1.end(), [](const vector<int>::iterator& it) { return *it > 4; }));
+    EXPECT_EQ(v1.end(), binary_search_pred(v1.begin(), v1.end(), [](const vector<int>::iterator& it) { return *it > 5; }));
+    EXPECT_EQ(v1.end(), binary_search_pred(v1.begin(), v1.end(), [](const vector<int>::iterator& it) { return *it >= 6; }));
 
-    EXPECT_EQ(v.begin() + 0, lower_bound(v.begin(), v.end(), 1, deref_it, 1));
-    EXPECT_EQ(v.begin() + 0, lower_bound(v.begin(), v.end(), 1, deref_it, 2));
-    EXPECT_EQ(v.begin() + 2, lower_bound(v.begin(), v.end(), 1, deref_it, 3));
-    EXPECT_EQ(v.begin() + 2, lower_bound(v.begin(), v.end(), 1, deref_it, 4));
-    EXPECT_EQ(v.begin() + 2, lower_bound(v.begin(), v.end(), 1, deref_it, 5));
-    EXPECT_EQ(v.begin() + 6, lower_bound(v.begin(), v.end(), 1, deref_it, 6));
-    EXPECT_EQ(v.begin() + 6, lower_bound(v.begin(), v.end(), 1, deref_it, 7));
-    EXPECT_EQ(v.begin() + 6, lower_bound(v.begin(), v.end(), 1, deref_it, 8));
-    EXPECT_EQ(v.begin() + 7, lower_bound(v.begin(), v.end(), 1, deref_it, 9));
-    EXPECT_EQ(v.end(), lower_bound(v.begin(), v.end(), 1, deref_it, 9));
+    EXPECT_EQ(v.begin() + 0, binary_search_pred(v.begin(), v.end(), [](const vector<int>::iterator& it) { return *it >= 1; }));
+    EXPECT_EQ(v.begin() + 0, binary_search_pred(v.begin(), v.end(), [](const vector<int>::iterator& it) { return *it >= 2; }));
+    EXPECT_EQ(v.begin() + 2, binary_search_pred(v.begin(), v.end(), [](const vector<int>::iterator& it) { return *it >= 3; }));
+    EXPECT_EQ(v.begin() + 2, binary_search_pred(v.begin(), v.end(), [](const vector<int>::iterator& it) { return *it >= 4; }));
+    EXPECT_EQ(v.begin() + 2, binary_search_pred(v.begin(), v.end(), [](const vector<int>::iterator& it) { return *it >= 5; }));
+    EXPECT_EQ(v.begin() + 6, binary_search_pred(v.begin(), v.end(), [](const vector<int>::iterator& it) { return *it >= 6; }));
+    EXPECT_EQ(v.begin() + 6, binary_search_pred(v.begin(), v.end(), [](const vector<int>::iterator& it) { return *it >= 7; }));
+    EXPECT_EQ(v.begin() + 6, binary_search_pred(v.begin(), v.end(), [](const vector<int>::iterator& it) { return *it >= 8; }));
+    EXPECT_EQ(v.begin() + 7, binary_search_pred(v.begin(), v.end(), [](const vector<int>::iterator& it) { return *it >= 9; }));
+    EXPECT_EQ(v.end(), binary_search_pred(v.begin(), v.end(), [](const vector<int>::iterator& it) { return *it >= 9; }));
 
-    EXPECT_EQ(v.begin() + 0, upper_bound(v.begin(), v.end(), 1, deref_it, 1));
-    EXPECT_EQ(v.begin() + 2, upper_bound(v.begin(), v.end(), 1, deref_it, 2));
-    EXPECT_EQ(v.begin() + 2, upper_bound(v.begin(), v.end(), 1, deref_it, 3));
-    EXPECT_EQ(v.begin() + 2, upper_bound(v.begin(), v.end(), 1, deref_it, 4));
-    EXPECT_EQ(v.begin() + 6, upper_bound(v.begin(), v.end(), 1, deref_it, 5));
-    EXPECT_EQ(v.begin() + 6, upper_bound(v.begin(), v.end(), 1, deref_it, 6));
-    EXPECT_EQ(v.begin() + 6, upper_bound(v.begin(), v.end(), 1, deref_it, 7));
-    EXPECT_EQ(v.begin() + 7, upper_bound(v.begin(), v.end(), 1, deref_it, 8));
-    EXPECT_EQ(v.begin() + 7, upper_bound(v.begin(), v.end(), 1, deref_it, 9));
-    EXPECT_EQ(v.end(), upper_bound(v.begin(), v.end(), 1, deref_it, 9));
+    EXPECT_EQ(v.begin() + 0, binary_search_pred(v.begin(), v.end(), [](const vector<int>::iterator& it) { return *it > 1; }));
+    EXPECT_EQ(v.begin() + 2, binary_search_pred(v.begin(), v.end(), [](const vector<int>::iterator& it) { return *it > 2; }));
+    EXPECT_EQ(v.begin() + 2, binary_search_pred(v.begin(), v.end(), [](const vector<int>::iterator& it) { return *it > 3; }));
+    EXPECT_EQ(v.begin() + 2, binary_search_pred(v.begin(), v.end(), [](const vector<int>::iterator& it) { return *it > 4; }));
+    EXPECT_EQ(v.begin() + 6, binary_search_pred(v.begin(), v.end(), [](const vector<int>::iterator& it) { return *it > 5; }));
+    EXPECT_EQ(v.begin() + 6, binary_search_pred(v.begin(), v.end(), [](const vector<int>::iterator& it) { return *it > 6; }));
+    EXPECT_EQ(v.begin() + 6, binary_search_pred(v.begin(), v.end(), [](const vector<int>::iterator& it) { return *it > 7; }));
+    EXPECT_EQ(v.begin() + 7, binary_search_pred(v.begin(), v.end(), [](const vector<int>::iterator& it) { return *it > 8; }));
+    EXPECT_EQ(v.begin() + 7, binary_search_pred(v.begin(), v.end(), [](const vector<int>::iterator& it) { return *it > 9; }));
+    EXPECT_EQ(v.end(), binary_search_pred(v.begin(), v.end(), [](const vector<int>::iterator& it) { return *it > 9; }));
 }
