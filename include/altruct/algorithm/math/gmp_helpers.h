d68436ca81bd543845350bc8a235a050c59a6c1c
Ognjen Dragoljevic <ognjen.dragoljevic@gmail.com>
2023-04-07 00:48:22 +0100

    gmp_helpers: cast mpf -> mpz, floor, ceil

--- include/altruct/algorithm/math/gmp_helpers.h
+++ include/altruct/algorithm/math/gmp_helpers.h
@@ -6,6 +6,9 @@
 #include "altruct/algorithm/math/base.h"
 #include "altruct/structure/math/modulo.h"
 
+#define ALTRUCT_USE_MPF_CLASS
+//#define ALTRUCT_USE_MPFR_CLASS
+
 typedef mpz_class mpz;
 typedef mpq_class mpq;
 typedef mpf_class mpf;
@@ -43,6 +46,11 @@ template<> mpz altruct::math::castT<mpz, unsigned long long>::of(const unsigned 
 template<> mpz altruct::math::castT<mpz, long long>::of(const long long& x) {
     return (x < 0) ? -castOf<mpz>((unsigned long long) - x) : castOf<mpz>((unsigned long long)x);
 };
+template<> mpz altruct::math::castT<mpz, mpf>::of(const mpf& x) {
+    mpz res;
+    mpz_set_f(res.get_mpz_t(), x.get_mpf_t());
+    return res;
+};
 
 //inline mpz int64_to_mpz(int64_t x) { return (mpz(int32_t(x >> 32)) << 32) | mpz(uint32_t(x & 0xFFFFFFFF)); }
 //inline int64_t mpz_to_int64(mpz x) { mpz xh = x >> 32, xl = x & 0xFFFFFFFF; return (int64_t(xh.get_si()) << 32) | uint64_t(xl.get_ui()); }
@@ -55,6 +63,8 @@ inline int64_t z_powmod(int64_t x, int64_t y, int64_t m) { return mpz_to_int64(z
 inline int is_prime(mpz x, int iter) { return mpz_probab_prime_p(x.get_mpz_t(), iter); }
 inline int is_prime(int64_t x, int iter) { return is_prime(int64_to_mpz(x), iter); }
 inline int64_t next_prime(int64_t x) { mpz p; mpz_nextprime(p.get_mpz_t(), int64_to_mpz(x).get_mpz_t()); return mpz_to_int64(p); }
+inline mpz z_div_floor(mpz a, mpz b) { mpz q; mpz_fdiv_q(q.get_mpz_t(), a.get_mpz_t(), b.get_mpz_t()); return q; }
+inline mpz z_div_ceil(mpz a, mpz b) { mpz q; mpz_cdiv_q(q.get_mpz_t(), a.get_mpz_t(), b.get_mpz_t()); return q; }
 inline mpz z_gcd(mpz x, mpz y) { mpz r; mpz_gcd(r.get_mpz_t(), x.get_mpz_t(), y.get_mpz_t()); return r; }
 inline mpz z_lcm(mpz x, mpz y) { return x * y / z_gcd(x, y); }
 inline mpz z_abs(mpz x) { mpz r; mpz_abs(r.get_mpz_t(), x.get_mpz_t()); return r; }
@@ -66,16 +76,18 @@ inline mpz z_pow(mpz x, int n) { mpz r; mpz_pow_ui(r.get_mpz_t(), x.get_mpz_t(),
 inline mpz z_root(mpz x, int n) { mpz r; mpz_root(r.get_mpz_t(), x.get_mpz_t(), n); return r; }
 inline mpz z_inverse(mpz x, mpz m) { mpz r; mpz_invert(r.get_mpz_t(), x.get_mpz_t(), m.get_mpz_t()); return r; }
 inline int z_testbit(mpz z, int i) { return mpz_tstbit(z.get_mpz_t(), i); }
+
+#ifdef ALTRUCT_USE_MPF_CLASS
 inline int f_int(mpf x) { return mpf_get_si(x.get_mpf_t()); }
 inline mpf f_sqrt(int n) { mpf r; mpf_sqrt_ui(r.get_mpf_t(), n); return r; }
 inline mpf f_sqrt(mpf f) { mpf r; mpf_sqrt(r.get_mpf_t(), f.get_mpf_t()); return r; }
 inline mpf f_pow(mpf f, int e) { mpf r; mpf_pow_ui(r.get_mpf_t(), f.get_mpf_t(), e); return r; }
 inline mpf f_abs(mpf x) { mpf r; mpf_abs(r.get_mpf_t(), x.get_mpf_t()); return r; }
 inline mpf f_floor(mpf x) { mpf r; mpf_floor(r.get_mpf_t(), x.get_mpf_t()); return r; }
+inline mpf f_ceil(mpf x) { mpf r; mpf_ceil(r.get_mpf_t(), x.get_mpf_t()); return r; }
 template<typename T> inline mpf& f_set(mpf &x, const T &val, int prec) { x.set_prec(prec); x = val; return x; }
 inline mpf& f_set(mpf &x, const mpf &val, int prec) { x.set_prec(prec ? prec : val.get_prec()); x = val; return x; }
-inline mpz z_div_floor(mpz a, mpz b) { mpz q; mpz_fdiv_q(q.get_mpz_t(), a.get_mpz_t(), b.get_mpz_t()); return q; }
-inline mpz z_div_ceil(mpz a, mpz b) { mpz q; mpz_cdiv_q(q.get_mpz_t(), a.get_mpz_t(), b.get_mpz_t()); return q; }
+#endif
 
 inline mpz operator % (const mpz& lhs, const int64_t& rhs) { return lhs % int64_to_mpz(rhs); }
 inline mpz operator % (const int64_t& lhs, const mpz& rhs) { return int64_to_mpz(lhs) % rhs; }
